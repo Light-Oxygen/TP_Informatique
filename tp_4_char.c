3c2cbc3dbd3735f3548b08f83c9633e1d67f3807
LightOxygen <43085973+Light-Oxygen@users.noreply.github.com>
2021-09-22 18:24:02 +0200

    Create tp_4_char.c

---
+++ tp_4_char.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned short Est_Une_Lettre (char c);
+
+int main()
+{
+    unsigned short verif;
+    char c;
+    printf("Saisir une lettre :\n");
+    fflush(stdin);
+    c=getchar();
+    verif = Est_Une_Lettre(c);
+    if(verif == 1) {
+        printf("C'est une lettre");
+    } else {
+        printf("C'est PAS une lettre");
+    }
+    return 0;
+}
+
+unsigned short Est_Une_Lettre (char c)
+{
+    unsigned short Result;
+    if (c >= 97 && c <= 122) {
+        Result = 1;
+    }
+    else if (c >=65 && c <= 90)
+    {
+        Result = 1;
+    }
+    else
+    {
+        Result = 0;
+    }
+
+    return Result;
+
+}
