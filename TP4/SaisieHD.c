76240a74bbc71813660917328bfc59fdcdbfa851
LightOxygen <43085973+Light-Oxygen@users.noreply.github.com>
2021-09-28 13:39:26 +0200

    30%

--- TP4/SaisieHD.c
+++ TP4/SaisieHD.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned short ValeurAbs (signed short x);
-signed short Saisie_hd(void);
+unsigned short ValeurAbs (signed short x); /* Prototypage de la fonction ValeurAbs, calculant la valeur absolue de la valeur x indiquée */
+signed short Saisie_hd(void); /* Prototypage de la fonction Saisie_Hd, récupérant un nombre entier */
 
 int main()
 {
 
+    /* Définition des trois valeurs numériques utilisées dans le main, Val1 et Val2 qui sont récupérées en entrée et Diff, la différence entre Val1 et Val2*/
     signed short Val1, Val2, Diff;
-    unsigned short AbsVal1, AbsVal2;
+    unsigned short AbsVal1, AbsVal2; /* Définition des variables stockant les valeurs absolues */
 
-    Val1 = Saisie_hd();
+    Val1 = Saisie_hd(); /* Récupération de Val1 avec la fonction définie Saisie_Hd() */
 
-    Val2 = Saisie_hd();
+    Val2 = Saisie_hd(); /* Récupération de Val2 avec la fonction définie Saisie_Hd() */
+    
     AbsVal1 = ValeurAbs(Val1);
     AbsVal2 = ValeurAbs(Val2);
 
